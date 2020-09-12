55be5da6d21364dcee1a84601569f11add81d4fa
MiladYarzaman1370 <yarzaman.milad@gmail.com>
2020-09-13 01:21:56 +0430

    init.

---
+++ services/business/miladYarzaman/week1_exercise_httpMethod_clientَAndserverApp/spring_repository/src/main/java/com/mapsa/spring_repository/SpringRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.mapsa.spring_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRepositoryApplication.class, args);
+    }
+
+}
